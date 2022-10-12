2a6f216f5938aa6177f8fce881b0c34ddab0e2e5
yashrawat21 <105967722+YashRawat21@users.noreply.github.com>
2022-10-12 10:40:05 +0000

    updated

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 var readLineSync = require('readline-sync');
 var score = 0 ;
-
+var highScore = 4;
+var finalScore = 0;
 function playQuiz(question,answer){
   var userAnswer = readLineSync.question(question);
-  if(userAnswer===answer){
+  if(userAnswer.toUpperCase() === answer.toUpperCase()){
     console.log(" You are correct ");
     score = score+1;
-    console.log(score);
+    console.log("Your score is : ", score);
+    finalScore = score;
     
   }else{
     console.log(" No your answer is incorrect ");
-      score = score-1;
-   console.log(score);
+    console.log("The correct answer is : ",answer)
+   console.log("Your score is : ", score);
    
   }
 }
-playQuiz(" what's your name ","Yash Rawat");
-playQuiz(" are you above 18 ","yes");
-playQuiz(" where do you live ","Delhi");
-playQuiz(" favourite hobby ","reading novels");
-playQuiz("favourite sport ", "football");
+playQuiz(" what's my name ? ","Yash");
+playQuiz(" Am I above 18 ? ","yes");
+playQuiz(" where do I live ? ","Delhi");
+playQuiz(" What I love to do in my free time ? ","reading");
+playQuiz(" What is my favourite sport ? ", "football");
+playQuiz(" Who is my favourite footballer ? ", "Lionel Messi")
 
+console.log("Your final score is : ",finalScore);
+if(highScore < finalScore) {
+  console.log("Congratulations, you have broken the high score.\n Send me the screenshot so that I can display your name.🏆 ")
+}
 
